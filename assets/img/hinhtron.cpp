d44a1ebe07b2b8ff43a1ba3deac0cdd3ccbe2052
tiendung1233 <nguyentiendung159357@gmail.com>
2021-09-30 21:30:34 +0700

    hello

---
+++ assets/img/hinhtron.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+ int main()
+ {
+ 	
+ 	double r;
+ 	float x=3.14;
+ 	cout<<"nhap vao ban kinh:";
+ 	cin>>r;
+ 	float d=r*2;
+ 	float S=x*r*r;
+ 	float P=2*x*r;
+ 	cout<<"duong kinh="<<d<<endl;
+ 	cout<<"dien tich="<<S<<endl;
+ 	cout<<"chu vi="<<P<<endl;
+ 	return 0;
+ 	}
